0579560136f85c0a0a058642ed6947a391949b12
Anastasiya Sachko <95384801+saachko@users.noreply.github.com>
2023-03-10 00:58:36 +0300

    Develop (#1)
    * init: start project
    
    * feat: create server
    
    * feat: add websockets connection
    
    * fix: fix UserData interface
    
    * fix: change the message data to clients
    
    * fix: fix users deletion
    
    * feat: add rawData.type
    
    * feat: add moves sending
    
    * fix: fix ws on close
    
    * fix: fix app work on socket close
    
    * docs: update readme

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import ws from 'ws';
+import { v4 } from 'uuid';
+
+import { ClientData, RawData, UserData, UserSocketData } from './interfaces';
+
+const clients: ClientData = {};
+let users: UserSocketData[] = [];
+
+const { Server } = ws;
+const wss = new Server(
+  {
+    port: 3001,
+  },
+  () => console.log(`server started`)
+);
+
+wss.on('connection', (ws) => {
+  const id = v4();
+  clients[id] = ws;
+
+  ws.on('message', (data) => {
+    const rawData: RawData = JSON.parse(data.toString());
+
+    switch (rawData.type) {
+      case 'user':
+        const user = rawData.user;
+        users.push({ ...user, clientId: id });
+        for (const id in clients) {
+          clients[id].send(JSON.stringify({ type: 'user', users }));
+        }
+        break;
+      case 'move':
+        for (const id in clients) {
+          clients[id].send(
+            JSON.stringify({
+              type: 'move',
+              moves: rawData.move,
+              opponentId: rawData.opponentId,
+            })
+          );
+        }
+        break;
+    }
+  });
+
+  ws.on('close', () => {
+    const deletedUsers = users.filter((user) => user.clientId === id);
+    users = users.filter((user) => user.clientId !== id);
+    for (const id in clients) {
+      clients[id].send(JSON.stringify({ type: 'close', users: deletedUsers }));
+    }
+    delete clients[id];
+  });
+});
---
+++ src/interfaces.ts
@@ -0,0 +1,25 @@
+import ws from 'ws';
+
+type DataType = 'user' | 'move' | 'close';
+interface UserData {
+  id: string;
+  username: string;
+  room: string;
+}
+
+interface UserSocketData extends UserData {
+  clientId: string;
+}
+
+interface RawData {
+  type: DataType;
+  user: UserData;
+  move: number[];
+  opponentId: string;
+}
+
+interface ClientData {
+  [key: string]: ws.WebSocket;
+}
+
+export { ClientData, UserData, UserSocketData, RawData };
